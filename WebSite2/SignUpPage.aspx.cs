685d20ef0f27b036c1c8cbdb0ed8a7c35f861ba4
kareem scott <kareem_scottt@hotmail.com>
2013-05-03 13:43:44 -0700

    update inden

--- WebSite2/SignUpPage.aspx.cs
+++ WebSite2/SignUpPage.aspx.cs
@@ -11,4 +11,8 @@ public partial class SignUpPage : System.Web.UI.Page
     {
 
     }
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        BAL.InsertPerson(new Identifiers.Person() { Addr = txtAddr.Text, CellNum = Convert.ToInt32(txtCell.Text), DOB = Convert.ToDateTime(txtDOB.Text), Email = txtEmail.Text, Fname = txtFname.Text, Lname = txtLname.Text, Gender = txtGender.Text, MiddleName = txtMname.Text, MotherMaiden=txtMaiden.Text,NAtionID=txtNatID.Text, TRN=TXTtrn.Text,Passport=txtPassport.Text});
+    }
 }
